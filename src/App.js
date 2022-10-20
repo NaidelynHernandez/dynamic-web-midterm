c7c7323c10751ca8632d9e91cce7691f8105981e
Naidelyn Hernandez <naidelynhernandez@Naidelyns-MacBook-Pro.local>
2022-10-19 21:11:25 -0400

    added new pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
+import Home from "./pages/Home"; 
+import     from "./pages/";
+import     from "./pages/";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <List/>,
+  },
+  {
+    path: "/article/:id",
+    element: <Article/>,
+  }, 
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="App">
+            <RouterProvider router={router} />
+
+     
+     </div>
   );
 }
+
 
 export default App;
