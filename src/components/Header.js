4a4ff3cf9886c56f71867bae0bafc5fa91f73e26
Naidelyn Hernandez <naidelynhernandez@Naidelyns-MacBook-Pro.local>
2022-10-27 01:17:21 -0400

    added search bar to query data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,30 @@
-import React from 'react'; 
-
-
-function Header (){
-
-    return <header> </header>;
-}
+//  import React from "react";
 
 
 
 
+// function Header(){
+// //    <form 
+// //     onSubmit= {handleSearchCountry}
+// //     >
+// //         <input 
+            
+
+// //            type="text"
+// //            name="search"
+// //            value={searchText}
+// //            placeholder="Input country "
+// //            onChange={(e) => setSearchText(e.target.value)}
+
+// //      />
+
+
+// //     </form>
+
+// }
 
 
 
 
 
-export default Header; 
+//   export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
-import React, {useEffect,useMemo,useState } from "react";
+
+import React, {useEffect,useState } from "react";
 import Countries from "../components/Countries";
 import axios from "axios";
-
-
+import Header from "../components/Header"
 
 function Home(){
 const [countryData,setData]= useState([]);
-
+//const [searchText, setSearchText ]= useState("");
 const astroURL='https://restcountries.com/v3.1/all'
-const flagURL='https://countryflagsapi.com/png/'
+//const flagURL='https://countryflagsapi.com/png/'
+const [searchText, setSearchText ]= useState("");
 
 
 useEffect(()=> {
@@ -30,26 +31,59 @@ useEffect(()=> {
 
 console.log("hehedata",countryData);
 
-// const {altSpellings,
-//        flags,
-//        } = useMemo(() => {
-//     const countryinfo= countryData.altSpellings || [];
-    
-//     return {
-//         altSpellings: countryinfo.altSpellings && countryinfo.altSpellings[0],
-//    };
 
-// }, [countryData]);
+async function searchCountry() {
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/name/${searchText}`
+      );
+      const data = await res.json();
+      setData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  function handleSearchCountry(e) {
+    e.preventDefault();
+    searchCountry();
+  }
 
 
 return (
     <div> 
+        <section>
+    <form 
+        onSubmit= {handleSearchCountry}
+        >
+            <input 
+                
+
+            type="text"
+            name="search"
+            value={searchText}
+            placeholder="Input country "
+            onChange={(e) => setSearchText(e.target.value)}
+
+        />
+
+    </form>
+            </section>
     <h1> Traveler's Guide</h1>
     {countryData.map((country,i) => (
        <Countries country={country} key={i}/> 
       
     ))}
-    </div>
+   
+
+    
+
+</div>
+
+
+
+
 ); 
 
 
@@ -58,3 +92,5 @@ return (
 export default Home;
     
     
+
+  
